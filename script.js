d735b3d7ad0c1504afaba0217f00386d8c05eb56
JuliaLabre <juliarlabres@gmail.com>
2022-10-03 15:02:27 -0300

    menu dropdown

--- script.js
+++ script.js
@@ -22,13 +22,19 @@
 function runApp() {
 
   // Carrega a página inicial do site quando este iniciar:
-  loadPage('policies');
+  loadPage('home');
 
   /**
    * jQuery → Quando houver click em um elemento <a>, execute o aplicativo 
    * "routerLink":
    **/
-   $('a').click(routerLink);
+  $(document).on('click', 'a', routerLink);
+
+  // Prepara o menu dropdown para exibição correta conforme a largura da tela:
+  resize();
+
+  // Se a largura da tela mudar durante a execução, reajusta o menu dropdown:
+  $(window).resize(resize);
 
 }
 
@@ -37,18 +43,33 @@ function runApp() {
  */
 function routerLink() {
 
+  // Quando clica em qualquer link, oculta o menu dropdown:
+  hideMenu();
+
   /**
-   * jQuery → Recebe o atributo "href" do link clicado e armazena em 'href':
-   * A função "$(this)" faz referência ao elemento que foi clicado e disparou 
-   * este processo.
+   * jQuery → Recebe o atributo (attr()) "href" do link clicado e armazena 
+   * em 'href'. A função "$(this)" faz referência ao elemento que foi clicado
+   * e disparou este processo.
    **/
   var href = $(this).attr('href');
 
+  // Se o link clicado é o botão do menu (href="menu")...
+  if (href == 'menu') {
+
+    // Chama a função que controla a exibição do menu dropdown:
+    toggleMenu();
+
+    // Sai de "routerLink()" sem fazer mais nada (false):
+    return false;
+  }
+
   /**
    * Se faz referência a link externo que começa com "http://" OU "https://",
-   * ou faz referência a uma âncora que começa com "#"...
+   * OU faz referência a uma âncora que começa com "#"...
    * 
    * OBS: O código "||" (pipe pipe) significa o "OU" (OR) lógico em JavaScript.
+   * Para OU (||) se apenas uma das subexpressões é verdadeira (true) toda a 
+   * expressão será verdadeira.
    * 
    * Referências: 
    *    https://youtu.be/mp3g9IQ651g
@@ -56,10 +77,10 @@ function routerLink() {
    *    https://www.w3schools.com/js/js_if_else.asp
    **/
   if (
-    // Se clicou em um link que começa com "http://...", OU
+    // Se clicou em um link que começa com "http://", OU
     href.substr(0, 7) == 'http://' ||
 
-    // Se clicou em um link que começa com "https://...", OU
+    // Se clicou em um link que começa com "https://", OU
     href.substr(0, 8) == 'https://' ||
 
     // Se clicou em uma âncora que começa com "#"...
@@ -67,8 +88,8 @@ function routerLink() {
   ) {
 
     /**
-     * Encerra o programa retornando "true" (verdade) para que o HTML abra o 
-     * link normalmente:
+     * Encerra "routerLink()" retornando "true" (verdade) para que o HTML abra 
+     * o link normalmente:
      **/
     return true;
   }
@@ -80,8 +101,8 @@ function routerLink() {
   loadPage(href);
 
   /**
-   * Encerra o programa retornando "false" (falso) para que a ação do HTML ao
-   * clicar no link seja bloqueada:
+   * Encerra "routerLink()" retornando "false" (falso) para que a ação do HTML
+   * ao clicar no link seja bloqueada:
    **/
   return false;
 }
@@ -121,7 +142,7 @@ function loadPage(href) {
 /**
  * setTitle() → Aplicativo que troca o <title> do documento conforme cada 
  * página é acessada. Para isso, inclua a chamada "setTitle('Titulo')" em cada
- * arquivo "script.js" de cada página.
+ * arquivo "script.js" de cada página dentro da pasta "pages".
  */
 function setTitle(title = '') {
 
@@ -142,7 +163,92 @@ function setTitle(title = '') {
 }
 
 /**
- * Cria o menu de navegação complementar de sobre:
+ * resize() → Aplicativo que ajusta o menu dropdown conforme a resolução 
+ * (width) da viewport. Temos em "index.html" 3 elementos a serem controlados:
+ *     • Os itens do menu normal, com a classe ".dropable";
+ *     • O botão que controla o menu, com id "#btnMenu";
+ *     • O menu dropdown em sí, com o id "#dropable".
+ */
+// Ajusta o menu dropdown:
+function resize() {
+
+  // jQuery → Oculta o menu:
+  $('#dropable').hide('fast');
+
+  // Se a largura da tela é maior que 574px...
+  if (window.innerWidth > 574) {
+
+    // jQuery → Oculta o botão do menu:
+    $('#btnMenu').hide(0);
+
+    // jQuery → Mostra o menu normal:
+    $('.dropable').show(0);
+
+    // Se não...
+  } else {
+
+    // jQuery → Oculta o menu normal:
+    $('.dropable').hide(0);
+
+    // jQuery → Mostra o botão do menu:
+    $('#btnMenu').show(0);
+
+  }
+}
+
+/**
+ * toggleMenu() → Aplicativo que controla a exibição do menu dropdown.
+ */
+function toggleMenu() {
+
+  // jQuery → Se o menu está visível...
+  if ($('#dropable').is(":visible")) {
+
+    // Chama a função que oculta o menu:
+    hideMenu();
+
+    // Se não...
+  } else {
+
+    // Chama a função que mostra o menu:
+    showMenu();
+  }
+}
+
+/**
+ * hideMenu() → Aplicativo que oculta o menu dropdown e também aplica o efeito
+ * de animação no ícone do botão de menu. A classe "fa-rotate-90" que gira o 
+ * ícone, faz parte da biblioteca "Font Awesome". Referências:
+ *     https://fontawesome.com/docs/web/style/rotate
+ */
+function hideMenu() {
+
+  // jQuery → Oculta o menu:
+  $('#dropable').hide('fast');
+
+  // jQuery → Remove rotação do ícone do botão do menu:
+  $('#btnMenu i').removeClass('fa-rotate-90');
+}
+
+/**
+ * showMenu() → Aplicativo que mostra o menu dropdown e também aplica o efeito
+ * de animação no ícone do botão de menu. 
+ */
+function showMenu() {
+
+  // jQuery → Mostra o menu:
+  $('#dropable').show('fast');
+
+  // jQuery → Rotaciona o ícone do botão do menu:
+  $('#btnMenu i').addClass('fa-rotate-90');
+}
+
+/**
+ * Menu de navegação complementar da seção "Sobre...", em HTML.
+ * 
+ * Como temos várias páginas nessa seção, não é necessário ficar repetindo o
+ * trecho de código abaixo para mostrar o menu em cada página, basta exibir o 
+ * valor da variável "aboutMenu" no elemento desejado.
  */
 var aboutMenu = `
 <a href="site"><i class="fa-solid fa-globe fa-fw"></i><span>Sobre o site</span></a>
